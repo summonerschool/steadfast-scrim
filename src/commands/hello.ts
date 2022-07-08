9323f28627189fa01a93c212ddca007998b27dba
Ricardo <ricardo@urbansquid.london>
2022-07-09 00:22:15 +0100

    lint:fix command

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,24 +1,26 @@
-import { SlashCommand, CommandOptionType, CommandContext } from "slash-create";
+import { SlashCommand, CommandOptionType, CommandContext } from 'slash-create';
 
 class HelloCommand extends SlashCommand {
   constructor(creator) {
     super(creator, {
       name: 'hello',
       description: 'Says hello to you.',
-      options: [{
-        type: CommandOptionType.STRING,
-        name: 'food',
-        description: 'What food do you like?'
-      }]
+      options: [
+        {
+          type: CommandOptionType.STRING,
+          name: 'food',
+          description: 'What food do you like?'
+        }
+      ]
     });
 
     this.filePath = __filename;
   }
 
   async run(ctx: CommandContext) {
-    console.info(ctx.user.id)
+    console.info(ctx.user.id);
     return ctx.options.food ? `You like ${ctx.options.food}? Nice!` : `Hello, ${ctx.user.id}(${ctx.user.username})!`;
   }
 }
 
-export default HelloCommand
+export default HelloCommand;
--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -1,27 +1,29 @@
 import { SlashCommand, CommandOptionType } from 'slash-create';
 
- class QueueCommand extends SlashCommand {
-    constructor(creator) {
-        super(creator, {
-            name: 'queue',
-            description: 'Queue',
-            options: [{
-                type: CommandOptionType.SUB_COMMAND,
-                name: 'join',
-                description: 'Here is the first sub command',
-            }, {
-                type: CommandOptionType.SUB_COMMAND,
-                name: 'leave',
-                description: 'Leave the thingie',
-                
-            }]
-        });
-        this.filePath = __filename;
-    }
-    async run(ctx) {
-        // returns the subcommand, option, and option value
-        return "Go and touch grass you loser"
-    }
-};
+class QueueCommand extends SlashCommand {
+  constructor(creator) {
+    super(creator, {
+      name: 'queue',
+      description: 'Queue',
+      options: [
+        {
+          type: CommandOptionType.SUB_COMMAND,
+          name: 'join',
+          description: 'Here is the first sub command'
+        },
+        {
+          type: CommandOptionType.SUB_COMMAND,
+          name: 'leave',
+          description: 'Leave the thingie'
+        }
+      ]
+    });
+    this.filePath = __filename;
+  }
+  async run(ctx) {
+    // returns the subcommand, option, and option value
+    return 'Go and touch grass you loser';
+  }
+}
 
-export default QueueCommand
+export default QueueCommand;
