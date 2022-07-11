57ec40860f52b717f284207e2b559736f7882c8a
Anhkha Vo <anhkhav@gmail.com>
2022-07-11 20:09:06 +0000

    start on message embed

---
+++ src/messages/match.ts
@@ -0,0 +1,26 @@
+import { MessageEmbed } from "discord.js";
+import { Scrim } from "../entities/scrim";
+
+
+const matchMessage= (scrim: Scrim) => {
+    return  new MessageEmbed()
+	.setColor('#698371')
+	.setTitle(`Match ${scrim.id}`)
+
+}
+const exampleEmbed = new MessageEmbed()
+
+	.setURL('https://discord.js.org/')
+	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+	.setDescription('NICE')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
