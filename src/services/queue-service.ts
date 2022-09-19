4aaabab363d191327910282ce166d927bf0cfd7e
Anhkha Vo <anhkhav@gmail.com>
2022-09-19 20:00:50 +0000

    remove user after 8 hours

--- src/services/queue-service.ts
+++ src/services/queue-service.ts
@@ -19,8 +19,12 @@ type Queues = {
   NA: Map<string, User>;
 };
 
+const HOUR = 3600000;
+const REMOVE_DURATION = HOUR * 8;
+
 export const initQueueService = (scrimService: ScrimService) => {
   const queues = new Map<string, Queues>();
+  const resetTimer = new Map<string, NodeJS.Timeout>();
 
   const service: QueueService = {
     joinQueue: (user, guildID, region) => {
@@ -28,19 +32,29 @@ export const initQueueService = (scrimService: ScrimService) => {
       if (queue[region].get(user.id)) {
         throw new Error("You're already in queue");
       }
-      const activeScrims = scrimService.getActiveScrims()
+      const activeScrims = scrimService.getActiveScrims();
       for (const scrim of activeScrims) {
-        if (scrim.players.some(p => p.userID === user.id)) {
+        if (scrim.players.some((p) => p.userID === user.id)) {
           throw new Error("You're already in a game. Please report the match before queuing up again.");
         }
       }
       queue[region] = queue[region].set(user.id, user);
       queues.set(guildID, queue);
+
+      // removes the user after 8 hours
+      resetTimer.set(
+        user.id,
+        setTimeout(() => {
+          service.leaveQueue(user.id, guildID, region);
+        }, REMOVE_DURATION)
+      );
       return [...queue[region].values()];
     },
     leaveQueue: (userID, guildID, region) => {
       const queue = queues.get(guildID);
       if (queue && queue[region].delete(userID)) {
+        clearTimeout(resetTimer.get(userID));
+        resetTimer.delete(userID);
         return [...queue[region].values()];
       } else {
         throw new Error("You're not in the specified queue");
@@ -64,7 +78,7 @@ export const initQueueService = (scrimService: ScrimService) => {
       if (queue) {
         queue[region].clear();
       }
-    },
+    }
   };
   return service;
 };
