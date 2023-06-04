92f523929834d07ae82a5b9e00dac6a4012297a9
kharann <hey@akvo.no>
2023-06-04 20:01:20 +0200

    Allow user to select division when setting up profile and remove mmr fetching

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,15 +1,11 @@
 import type { PrismaClient, User } from '@prisma/client';
-import axios from 'axios';
 import { NotFoundError } from '../errors/errors';
-import type { WhatIsMyMMRResponse } from '../models/external';
 import type { SetupInput } from '../schemas/user';
-import { ELO_TRANSLATION } from '../utils/utils';
 
 export interface UserService {
   setUserProfile(id: string, input: SetupInput, elo: number): Promise<User>;
   updateElo(id: string, elo?: number, externalElo?: number): Promise<User>;
   getUserProfile(id: string): Promise<User>;
-  fetchMyMMR(server: string, leagueIGN: string): Promise<{ rank: string; elo: number }>;
   getUsers(ids: string[]): Promise<User[]>;
 }
 
@@ -39,25 +35,6 @@ export class UserServiceImpl implements UserService {
     const user = await this.prisma.user.findUnique({ where: { id } });
     if (!user) throw new NotFoundError(`User(<@${id}>) does not have a profile. Please use /setup`);
     return user;
-  }
-
-  async fetchMyMMR(server: string, leagueIGN: string): Promise<{ rank: string; elo: number }> {
-    const res = await axios.get<WhatIsMyMMRResponse | Error>(
-      `https://${server.toLowerCase()}.whatismymmr.com/api/v1/summoner?name=${leagueIGN}`
-    );
-
-    if ('message' in res.data) {
-      throw new NotFoundError(res.data.message);
-    }
-    const mymmr = res.data;
-
-    const rank = mymmr.ranked.closestRank.split(' ')[0].toUpperCase();
-    const elo = mymmr.ranked.avg || ELO_TRANSLATION[rank];
-
-    return {
-      rank: rank,
-      elo: elo
-    };
   }
 
   async getUsers(ids: string[]): Promise<User[]> {
